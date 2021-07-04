152d22e6aa17c494e0a705379389ba8b9bc533f6
Bartosz Kotelczuk <bartosz.kotelczuk@qunis.de>
2021-07-04 11:22:33 +0200

    training data

--- the-smart-mill-game/mill-gym/PPO_trainingcenter.py
+++ the-smart-mill-game/mill-gym/PPO_trainingcenter.py
@@ -3,14 +3,18 @@ from src.env.MillEnv_Descrete import MillEnviroment
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
+from stable_baselines3.common.callbacks import CheckpointCallback
+# Save a checkpoint every 1000 steps
+checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./checkpoints/sb/ppo',
+                                         name_prefix='ppo_checkpoint_model')
 
 env = MillEnviroment()
 vec_env = make_vec_env(lambda: env, n_envs=4)
 
 model = PPO("MlpPolicy", vec_env, verbose=1, tensorboard_log="./tensorboards/sb/", device='auto')
-model.learn(total_timesteps=int(9.5e+6))
+model.learn(total_timesteps=int(10e+10), callback=checkpoint_callback)
 
 
-model.save("./models/sb/ppo/model_ppo")
+model.save("./models/sb/ppo/model_ppo_2")
 
 print("____ AGENT LEARNED ___________")
